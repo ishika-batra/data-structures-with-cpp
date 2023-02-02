c2f579a1189de045213ace3731f5dc04f11b034f
ishika2203 <ishikabatra.net@gmail.com>
2023-02-02 23:04:06 +0530

    structures code added

--- 02-cpp-fundamentals/Structures.cpp
+++ 02-cpp-fundamentals/Structures.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
+using namespace std;
+
 struct Rectangle
 {
+  int length;
+  int breadth;
+  char x;
   /* data */
 };
+
+int main()
+{
+  struct Rectangle r1 = {10, 5};
+  cout << "Size of structure r1: " << sizeof(r1) << endl; // Output should be 9 be it's 12, This adjustment in the memory is called padding.
+  cout << "Length of Rectangle r1: " << r1.length << endl;
+  cout << "Breadth of Rectangle r1: " << r1.breadth << endl;
+  r1.length = 15;
+  r1.breadth = 7;
+  cout << " New Length of Rectangle r1: " << r1.length << endl;
+  cout << " New Breadth of Rectangle r1: " << r1.breadth << endl;
+}
