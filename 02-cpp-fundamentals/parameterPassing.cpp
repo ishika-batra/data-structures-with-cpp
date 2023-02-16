a9b6497c47b6b91052b1ff51dd24ed326356a5db
ishika2203 <ishikabatra.net@gmail.com>
2023-02-16 23:30:33 +0530

    parameter passing code added

--- 02-cpp-fundamentals/parameterPassing.cpp
+++ 02-cpp-fundamentals/parameterPassing.cpp
@@ -1,4 +1,50 @@
 #include <iostream>
 using namespace std;
+/*CALL BY VALUE
+void swap(int x, int y) // 10 and 20 passed in formal parameters
 {
+  int temp;
+  temp = x;
+  x = y;
+  y = temp; // x=20 y=10
 }
+int main()
+{
+  int a = 10;
+  int b = 20;
+  swap(a, b);
+  cout << a << " " << b; // a=10, b=20
+} // Not swapped
+*/
+/*CALL BY ADDRESS
+void swap(int *x, int *y) // 10 and 20 passed in formal parameters
+{
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp; // x=20 y=10
+}
+int main()
+{
+  int a = 10;
+  int b = 20;
+  swap(&a, &b);
+  cout << a << " " << b; // a=20, b=10
+} // Values got swapped
+*/
+/* CALL BY REFERENCE
+void swap(int &x, int &y) // 10 and 20 passed in formal parameters
+{
+  int temp;
+  temp = x;
+  x = y;
+  y = temp; // x=20 y=10
+}
+int main()
+{
+  int a = 10;
+  int b = 20;
+  swap(a, b);
+  cout << a << " " << b; // a=20, b=10
+} // swapped
+*/
